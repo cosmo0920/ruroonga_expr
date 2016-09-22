b5f6514e415df253075a3a406d5c27063f5a840e
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-09-22 16:32:08 +0900

    example: Use dsl once more

--- examples/grouped_expr.rs
+++ examples/grouped_expr.rs
@@ -1,55 +1,52 @@
 extern crate ruroonga_expr as expr;
 
-use expr::fulltext_expr::FulltextExpr;
-use expr::phrase_expr::PhraseExpr;
-use expr::greater_equal_expr::GreaterEqualExpr;
+use expr::dsl::*;
 
 fn logical_and_expr() {
-    let lexpr = FulltextExpr::new("Rust").column("language").prepare();
-    let rexpr = FulltextExpr::new("Haskell").column("language").prepare();
-    let comb_lexpr = GreaterEqualExpr::new("n_likes", "10").prepare();
+    let lexpr = fulltext_expr("Rust").column("language").prepare();
+    let rexpr = fulltext_expr("Haskell").column("language").prepare();
+    let comb_lexpr = greater_equal_expr("n_likes", "10").prepare();
     let result = (comb_lexpr % (lexpr + rexpr)).build();
     println!("Grouped Logical And: {}", result);
 }
 
 fn logical_or_expr() {
-    let lexpr = FulltextExpr::new("Rust").column("language").prepare();
-    let rexpr = FulltextExpr::new("Haskell").column("language").prepare();
-    let comb_lexpr = GreaterEqualExpr::new("n_likes", "10").prepare();
+    let lexpr = fulltext_expr("Rust").column("language").prepare();
+    let rexpr = fulltext_expr("Haskell").column("language").prepare();
+    let comb_lexpr = greater_equal_expr("n_likes", "10").prepare();
     let result = (comb_lexpr % (lexpr | rexpr)).build();
     println!("Grouped Logical OR: {}", result);
 }
 
 fn logical_not_expr() {
-    let lexpr = FulltextExpr::new("Rust").column("language").prepare();
-    let rexpr = FulltextExpr::new("Haskell").column("language").prepare();
-    let comb_lexpr = GreaterEqualExpr::new("n_likes", "10").prepare();
+    let lexpr = fulltext_expr("Rust").column("language").prepare();
+    let rexpr = fulltext_expr("Haskell").column("language").prepare();
+    let comb_lexpr = greater_equal_expr("n_likes", "10").prepare();
     let result = (comb_lexpr % (lexpr - rexpr)).build();
     println!("Grouped Logical Not: {}", result);
 }
 
 fn nested_logicals_expr() {
     use expr::groupable::Groupable;
-    use expr::groupable::logical_and_builder::LogicalAndBuilder;
 
-    let llexpr = PhraseExpr::new("Rust lang").column("language").prepare();
-    let lrexpr = PhraseExpr::new("Haskell lang").column("language").prepare();
-    let lcomb_lexpr = GreaterEqualExpr::new("n_likes", "10").prepare();
+    let llexpr = phrase_expr("Rust lang").column("language").prepare();
+    let lrexpr = phrase_expr("Haskell lang").column("language").prepare();
+    let lcomb_lexpr = greater_equal_expr("n_likes", "10").prepare();
     let llogical_expr = lcomb_lexpr % (llexpr | lrexpr);
     assert_eq!(concat!("\'n_likes:>=10 ",
                        "(language:@\"Rust lang\" OR language:@\"Haskell lang\")\'"),
                llogical_expr.clone().build());
 
-    let rlexpr = PhraseExpr::new("Ruby lang").column("language").prepare();
-    let rrexpr = PhraseExpr::new("Python lang").column("language").prepare();
-    let rcomb_lexpr = GreaterEqualExpr::new("n_likes", "10").prepare();
+    let rlexpr = phrase_expr("Ruby lang").column("language").prepare();
+    let rrexpr = phrase_expr("Python lang").column("language").prepare();
+    let rcomb_lexpr = greater_equal_expr("n_likes", "10").prepare();
     let rlogical_expr = rcomb_lexpr % (rlexpr | rrexpr);
     assert_eq!(concat!("\'n_likes:>=10 ",
                        "(language:@\"Ruby lang\" OR language:@\"Python lang\")\'"),
                rlogical_expr.clone().build());
 
-    let result = LogicalAndBuilder::new(llogical_expr.to_group(),
-                                        rlogical_expr.to_group()).build();
+    let result = logical::and(llogical_expr.to_group(),
+                              rlogical_expr.to_group()).build();
     println!("Nested logicals expr: {}", result);
 }
 
