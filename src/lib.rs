ca487d42d93a0347df6ace026b2d975fc6af1814
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-09-27 20:03:39 +0900

    Add rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -9,16 +9,27 @@ mod exprable;
 #[macro_use]
 pub mod macros;
 pub mod expr;
+/// Create Fulltext grn_expr.
 pub mod fulltext_expr;
+/// Create Phrase grn_expr.
 pub mod phrase_expr;
+/// Create Prefix grn_expr.
 pub mod prefix_expr;
+/// Create Suffix grn_expr.
 pub mod suffix_expr;
+/// Create Equal grn_expr.
 pub mod equal_expr;
+/// Create Not Equal grn_expr.
 pub mod notequal_expr;
+/// Create Less grn_expr.
 pub mod less_expr;
+/// Create Greater grn_expr.
 pub mod greater_expr;
+/// Create Less Equal grn_expr.
 pub mod less_equal_expr;
+/// Create Greater Equal grn_expr.
 pub mod greater_equal_expr;
+/// Create Match grn_expr.
 pub mod match_expr;
 pub mod groupable;
 pub mod dsl;
